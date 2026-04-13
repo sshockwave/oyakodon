32f3abb9255265af213711d3da0333687566d97b
Kai Su <16837579+sshockwave@users.noreply.github.com>
2026-04-13 15:19:38 +0800

    feat: add `impl`s for `BowlBox`

--- src/bowl_box.rs
+++ src/bowl_box.rs
@@ -1,5 +1,5 @@
-use super::{BowlMut, Derive};
-use ::alloc::boxed::Box;
+use super::{BowlMut, Derive, bowl_mut::Map};
+use ::{alloc::boxed::Box, core::mem::transmute};
 
 #[repr(transparent)]
 pub struct BowlBox<'a, T, F>(BowlMut<'a, Box<T>, F>)
@@ -50,5 +50,94 @@ where
         derive: Box<dyn for<'c> FnOnce(&'c mut T) -> <F as Derive<&'c mut T>>::Output>,
     ) -> Self {
         Self(BowlMut::new_into(Box::new(base), derive))
+    }
+
+    pub fn map_into<'b, G, H>(self, f: H) -> BowlBox<'b, T, G>
+    where
+        for<'c> H: Derive<<F as Derive<&'c mut T>>::Output>,
+        for<'c> G:
+            Derive<&'c mut T, Output = <H as Derive<<F as Derive<&'c mut T>>::Output>>::Output>,
+    {
+        BowlBox(self.0.map_into(f))
+    }
+
+    pub fn map<G>(self, f: G) -> BowlBox<'a, T, Map<T, F, G>>
+    where
+        G: for<'b> Derive<<F as Derive<&'b mut T>>::Output>,
+    {
+        self.map_into(f)
+    }
+
+    pub fn cast_ref<'b, G>(&self) -> &BowlBox<'b, T, G>
+    where
+        for<'c> G: Derive<&'c mut T, Output = <F as Derive<&'c mut T>>::Output>,
+    {
+        // SAFETY: Same as BowlMut::cast_ref()
+        unsafe { transmute(self) }
+    }
+    pub fn cast_mut<'b, G>(&mut self) -> &mut BowlBox<'b, T, G>
+    where
+        for<'c> G: Derive<&'c mut T, Output = <F as Derive<&'c mut T>>::Output>,
+    {
+        // SAFETY: Same as BowlMut::cast_mut()
+        unsafe { transmute(self) }
+    }
+    pub fn cast<'b, G>(self) -> BowlBox<'b, T, G>
+    where
+        for<'c> G: Derive<&'c mut T, Output = <F as Derive<&'c mut T>>::Output>,
+    {
+        self.0.cast().into()
+    }
+
+    pub fn get(&self) -> &<F as Derive<&'_ mut T>>::Output {
+        self.0.get()
+    }
+    pub fn get_mut(&mut self) -> &mut <F as Derive<&'_ mut T>>::Output {
+        self.0.get_mut()
+    }
+}
+
+impl<'a, T, F> Clone for BowlBox<'a, T, F>
+where
+    F: for<'b> Derive<&'b mut T>,
+    BowlMut<'a, Box<T>, F>: Clone,
+{
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<'a, T, F> From<BowlMut<'a, Box<T>, F>> for BowlBox<'a, T, F>
+where
+    F: for<'b> Derive<&'b mut T>,
+{
+    fn from(value: BowlMut<'a, Box<T>, F>) -> Self {
+        Self(value)
+    }
+}
+
+impl<'a, T, F> From<BowlBox<'a, T, F>> for BowlMut<'a, Box<T>, F>
+where
+    F: for<'b> Derive<&'b mut T>,
+{
+    fn from(value: BowlBox<'a, T, F>) -> Self {
+        value.0
+    }
+}
+
+#[cfg(feature = "gat")]
+impl<'a, T, F> super::Bowl for BowlBox<'a, T, F>
+where
+    F: for<'b> Derive<&'b mut T>,
+{
+    type Value<'b>
+        = <F as Derive<&'b mut T>>::Output
+    where
+        Self: 'b;
+    fn get(&self) -> &Self::Value<'_> {
+        self.0.get()
+    }
+    fn get_mut(&mut self) -> &mut Self::Value<'_> {
+        self.0.get_mut()
     }
 }
