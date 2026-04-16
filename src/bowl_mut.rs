e2eabfc786854e8ed7eddb1e37f433b3272690a4
Kai Su <16837579+sshockwave@users.noreply.github.com>
2026-04-16 14:14:45 +0800

    doc: add type API

--- src/bowl_mut.rs
+++ src/bowl_mut.rs
@@ -31,6 +31,27 @@ where
     type Output = <F as View<&'a mut T>>::Output;
 }
 
+/// Stores a pointer to an owner and a derived mutable reference into it.
+///
+/// The difference from [`BowlRef`] is that `derive` receives `&mut T::Target`.
+/// Because `&mut` implies exclusive access, the owner is not accessible before the view is dropped.
+/// This gives a set of traits different from [`BowlRef`].
+/// If you would like to have access to the owner after deriving the view,
+/// please store it in the view.
+///
+/// # Examples
+///
+/// ```
+/// use oyakodon::BowlMut;
+///
+/// fn parse_words(s: &mut String) -> Vec<&str> {
+///     s.split_whitespace().collect()
+/// }
+///
+/// let mut bowl = BowlMut::new(Box::new("hello world".to_owned()), parse_words);
+/// bowl.get_mut()[0] = "hi";
+/// assert_eq!(*bowl.get(), vec!["hi", "world"]);
+/// ```
 pub struct BowlMut<'a, T: Deref, F: ?Sized>(BowlRef<'a, T, MutToRef<F>>)
 where
     F: for<'b> View<&'b mut T::Target>;
@@ -50,6 +71,7 @@ where
     T: StableDeref + DerefMut,
     F: for<'b> View<&'b mut T::Target> + ?Sized,
 {
+    /// See [`BowlRef::from_derive`]. The only difference is that `derive` receives `&mut T::Target`.
     pub fn from_derive(
         owner: T,
         derive: impl for<'b> Derive<&'b mut T::Target, Output = <F as View<&'b mut T::Target>>::Output>,
@@ -66,6 +88,7 @@ where
         })
     }
 
+    /// See [`BowlRef::from_fn`].
     pub fn from_fn<'b>(
         owner: T,
         derive: &'b dyn for<'c> Fn(&'c mut T::Target) -> <F as View<&'c mut T::Target>>::Output,
@@ -76,6 +99,7 @@ where
         Self::from_derive(owner, derive)
     }
 
+    /// See [`BowlRef::from_fn_mut`].
     pub fn from_fn_mut<'b>(
         owner: T,
         derive: &'b mut dyn for<'c> FnMut(
@@ -88,6 +112,7 @@ where
         Self::from_derive(owner, derive)
     }
 
+    /// See [`BowlRef::from_fn_once`].
     #[cfg(feature = "alloc")]
     pub fn from_fn_once(
         owner: T,
@@ -98,6 +123,7 @@ where
         Self::from_derive(owner, derive)
     }
 
+    /// See [`BowlRef::map_into`].
     pub fn map_into<'b, G: ?Sized, H>(self, f: H) -> BowlMut<'b, T, G>
     where
         for<'c> H: Derive<<F as View<&'c mut T::Target>>::Output>,
@@ -109,6 +135,7 @@ where
         BowlMut(self.0.map_into(f))
     }
 
+    /// See [`BowlRef::map`].
     pub fn map<G>(self, f: G) -> BowlMut<'a, T, Map<T::Target, F, G>>
     where
         G: for<'b> Derive<<F as View<&'b mut T::Target>>::Output>,
@@ -144,10 +171,12 @@ where
     T: Deref,
     F: for<'b> View<&'b mut T::Target> + ?Sized,
 {
+    /// See [`BowlRef::cast_life`].
     pub fn cast_life<'b>(self) -> BowlMut<'b, T, F> {
         BowlMut(self.0.cast_life())
     }
 
+    /// See [`BowlRef::cast_view`].
     pub fn cast_view<G: ?Sized>(self) -> BowlMut<'a, T, G>
     where
         for<'b> G: View<&'b mut T::Target, Output = <F as View<&'b mut T::Target>>::Output>,
@@ -155,6 +184,7 @@ where
         BowlMut(self.0.cast_view())
     }
 
+    /// See [`BowlRef::cast`].
     pub fn cast<'b, G: ?Sized>(self) -> BowlMut<'b, T, G>
     where
         for<'c> G: View<&'c mut T::Target, Output = <F as View<&'c mut T::Target>>::Output>,
@@ -162,10 +192,12 @@ where
         BowlMut(self.0.cast())
     }
 
+    /// See [`BowlRef::into_owner`].
     pub fn into_owner(self) -> T {
         self.0.into_owner()
     }
 
+    /// See [`BowlRef::into_view`].
     pub fn into_view<S>(self) -> S
     where
         for<'c> F: View<&'c mut T::Target, Output = S>,
@@ -173,6 +205,7 @@ where
         self.0.into_view()
     }
 
+    /// See [`BowlRef::into_parts`].
     pub fn into_parts<S>(self) -> (T, S)
     where
         for<'c> F: View<&'c mut T::Target, Output = S>,
@@ -180,6 +213,7 @@ where
         self.0.into_parts()
     }
 
+    /// See [`BowlRef::into_async`].
     pub async fn into_async(self) -> BowlMut<'a, T, Async<T::Target, F>>
     where
         for<'c> <F as View<&'c mut T::Target>>::Output: Future,
@@ -187,6 +221,7 @@ where
         BowlMut(self.0.into_async().await.cast_view())
     }
 
+    /// See [`BowlRef::into_result`].
     pub fn into_result(
         self,
     ) -> Result<BowlMut<'a, T, Success<T::Target, F>>, BowlMut<'a, T, Failure<T::Target, F>>>
--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,8 @@ pub use bowl_ref::BowlRef;
 #[cfg(not(feature = "stable_deref"))]
 pub use stable_deref::{CloneStableDeref, StableDeref};
 
+/// Unified interface for all bowl types.
+/// [`BowlRef`], [`BowlMut`], and [`BowlBox`] all implement this trait.
 #[cfg(feature = "gat")]
 pub trait Bowl {
     type Value<'a>
@@ -28,10 +30,26 @@ pub trait Bowl {
     fn get_mut(&mut self) -> &mut Self::Value<'_>;
 }
 
+/// Marker trait for indicating the type of view.
+/// Its lifetime is dependent on the owner type,
+/// so it is actually a higher-kinded type
+/// that takes a lifetime parameter.
+///
+/// The purpose of type parameter `T` instead of a simple lifetime `'a` is twofold:
+/// 1. It allows us to define the `View` trait for all functions
+/// without triggering [E0207](https://doc.rust-lang.org/error_codes/E0207.html).
+/// 2. Writing `&'a T` constrains the lifetime variable in HRTB to at most as long as `T`.
 pub trait View<T> {
     type Output;
 }
 
+/// The consuming counterpart of [`View`].
+///
+/// Named functions automatically satisfy this via the blanket impl for [`FnOnce`].
+/// For closures, prefer the `from_fn` constructors with a `&dyn Fn` argument,
+/// since closures cannot express a generic return lifetime on stable Rust.
+/// Alternatively, define a custom type and implement [`View`] and [`Derive`] manually
+/// for zero-cost dispatch without the `dyn` indirection.
 pub trait Derive<T>: View<T> {
     fn call(self, input: T) -> Self::Output;
 }
@@ -52,6 +70,11 @@ where
     }
 }
 
+/// Phantom view type representing the composition of `F` followed by `G`.
+///
+/// Used as the view type parameter after calling [`BowlRef::map`], [`BowlMut::map`],
+/// or [`BowlBox::map`]. `F` is applied first to produce an intermediate value,
+/// which `G` then transforms into the final output.
 pub struct Map<T: ?Sized, F: ?Sized, G: ?Sized>(PhantomData<T>, PhantomData<F>, PhantomData<G>);
 
 impl<'a, T: ?Sized, F, G> View<&'a T> for Map<T, F, G>
@@ -70,6 +93,11 @@ where
     type Output = <G as View<<F as View<&'a mut T>>::Output>>::Output;
 }
 
+/// Phantom view type that awaits the future produced by `F`.
+///
+/// Used as the view type parameter after calling [`BowlRef::into_async`],
+/// [`BowlMut::into_async`], or [`BowlBox::into_async`].
+/// The owner remains alive for the entire duration of the await.
 pub struct Async<T: ?Sized, F: ?Sized>(PhantomData<T>, PhantomData<F>);
 
 impl<'a, T: ?Sized, F> View<&'a T> for Async<T, F>
@@ -88,6 +116,9 @@ where
     type Output = <<F as View<&'a mut T>>::Output as Future>::Output;
 }
 
+/// Abstracts over [`Result`] and [`Option`] for use with the `into_result` methods.
+///
+/// For [`Option<T>`], the error type is `()`, so [`None`] becomes [`Err(())`][Err].
 pub trait Outcome {
     type Ok;
     type Err;
@@ -110,6 +141,9 @@ impl<T, E> Outcome for Result<T, E> {
     }
 }
 
+/// Phantom view type for the `Ok` branch produced by `into_result`.
+///
+/// The contained view is [`Outcome::Ok`] of the original view type.
 pub struct Success<T: ?Sized, F: ?Sized>(PhantomData<T>, PhantomData<F>);
 
 impl<'a, T: ?Sized, F> View<&'a T> for Success<T, F>
@@ -128,6 +162,7 @@ where
     type Output = <<F as View<&'a mut T>>::Output as Outcome>::Ok;
 }
 
+/// Phantom view type for the `Err` branch produced by `into_result`.
 pub struct Failure<T: ?Sized, F: ?Sized>(PhantomData<T>, PhantomData<F>);
 
 impl<'a, T: ?Sized, F> View<&'a T> for Failure<T, F>
