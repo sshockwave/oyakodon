6ac73daf10126625ddecf816a82f6bd9782f4ac8
Kai Su <16837579+sshockwave@users.noreply.github.com>
2026-04-27 14:47:34 +0800

    refactor: use built-in `Deref` and local type

--- src/primitive/aliasable.rs
+++ src/primitive/aliasable.rs
@@ -1,8 +1,34 @@
-pub unsafe trait AliasableDeref {
-    type Target: ?Sized;
-    fn deref(&self) -> &Self::Target;
+use crate::StableDeref;
+use ::{
+    core::ops::{Deref, DerefMut},
+    maybe_dangling::MaybeDangling,
+};
+
+pub unsafe trait Aliasable: Deref {}
+
+pub struct DanglingDeref<T>(MaybeDangling<T>);
+
+impl<T> DanglingDeref<T> {
+    pub fn new(inner: T) -> Self {
+        Self(MaybeDangling::new(inner))
+    }
+
+    pub fn into_inner(self) -> T {
+        MaybeDangling::into_inner(self.0)
+    }
 }
 
-pub unsafe trait AliasableDerefMut: AliasableDeref {
-    fn deref_mut(&mut self) -> &mut Self::Target;
+impl<T: Deref> Deref for DanglingDeref<T> {
+    type Target = T::Target;
+    fn deref(&self) -> &Self::Target {
+        &*self.0
+    }
 }
+
+impl<T: DerefMut> DerefMut for DanglingDeref<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut *self.0
+    }
+}
+
+unsafe impl<T> Aliasable for DanglingDeref<T> where T: StableDeref {}
