0fa1d1f9d9d76840de256b339a05aec8f37e7127
Kai Su <16837579+sshockwave@users.noreply.github.com>
2026-05-06 11:15:31 +0800

    refactor: migrate the covariant example to `Bowl`

--- examples/covariant.rs
+++ examples/covariant.rs
@@ -1,25 +1,23 @@
 //! An example of using covariant views to get a direct reference to the view
-//! without wrapping everything in a function inside [`BowlRef::spawn`].
+//! without wrapping everything in a function inside [`Bowl::spawn`].
 
-use oyakodon::{BowlRef, Derive, View};
-use std::marker::PhantomData;
+use ::{
+    aliasable::boxed::AliasableBox,
+    oyakodon::primitive::{Bowl, View},
+};
 
 trait Covariant<'ub> {
-    type Value<'a>;
+    type Value<'a>
+    where
+        'ub: 'a;
     fn as_ref<'a, 'b, 'c>(value: &'c Self::Value<'a>) -> &'c Self::Value<'b>
     where
         'a: 'b;
 }
 
-struct CovariantView<'ub, F>(PhantomData<&'ub ()>, PhantomData<F>);
-impl<'a, 'ub, T: ?Sized, F> View<&'a T> for CovariantView<'ub, F>
-where
-    F: Covariant<'ub>,
-{
-    type Output = F::Value<'a>;
-}
-
-struct SelfRef<'ub, T, F>(BowlRef<'ub, Box<T>, CovariantView<'ub, F>>)
+struct SelfRef<'ub, T, F>(
+    Bowl<'ub, AliasableBox<T>, dyn for<'x> View<'x, Output = F::Value<'x>> + 'static>,
+)
 where
     T: ?Sized,
     F: Covariant<'ub>;
@@ -29,31 +27,16 @@ impl<'ub, T: ?Sized, F: Covariant<'ub>> SelfRef<'ub, T, F> {
     where
         F::Value<'a>: 'a,
     {
-        struct Shorten<'ub, F, T: ?Sized>(PhantomData<&'ub ()>, PhantomData<F>, PhantomData<T>);
-        impl<'a, 'ub, T: ?Sized, F: Covariant<'ub>> View<&'a F::Value<'_>> for Shorten<'ub, F, T>
-        where
-            F::Value<'a>: 'a,
-        {
-            type Output = &'a F::Value<'a>;
-        }
-        impl<'a, 'b, 'ub, T: ?Sized, F: Covariant<'ub>> Derive<&'a F::Value<'b>, &'a &'b ()>
-            for Shorten<'ub, F, T>
-        where
-            F::Value<'a>: 'a,
-        {
-            fn call(self, value: &'a F::Value<'b>) -> &'a F::Value<'a> {
-                F::as_ref(value)
-            }
-        }
-
-        self.0
-            .spawn(Shorten::<'ub, F, T>(PhantomData, PhantomData, PhantomData))
+        self.0.with(|view, _| F::as_ref(view))
     }
 }
 
 struct StrRef;
-impl Covariant<'static> for StrRef {
-    type Value<'a> = &'a str;
+impl<'ub> Covariant<'ub> for StrRef {
+    type Value<'a>
+        = &'a str
+    where
+        'ub: 'a;
     fn as_ref<'a, 'b, 'c>(value: &'c &'a str) -> &'c &'b str
     where
         'a: 'b,
@@ -65,10 +48,10 @@ impl Covariant<'static> for StrRef {
 }
 
 fn main() {
-    let cell = SelfRef(BowlRef::<Box<String>, CovariantView<StrRef>>::from_derive(
-        Box::new(String::from("hello, world")),
-        String::as_str,
-    ));
+    let cell = SelfRef::<_, StrRef>(Bowl::new_box(String::from("hello, world")).map(|sess| {
+        let (view, slot) = sess.open(|view, stamp| stamp.stamp(view.as_str()));
+        slot.fill(view)
+    }));
     let view = cell.get();
     println!("{view}");
 }
