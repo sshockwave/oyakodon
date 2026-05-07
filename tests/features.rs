423baa379f189714fa8b013f286d4415cad9193e
Kai Su <16837579+sshockwave@users.noreply.github.com>
2026-05-07 23:56:26 +0800

    test: support owner swap

--- tests/features.rs
+++ tests/features.rs
@@ -1,4 +1,4 @@
-use oyakodon::primitive::Bowl;
+use oyakodon::primitive::{Bowl, ProtectedForAll, View};
 
 #[test]
 fn owner_nonstatic_lifetime() {
@@ -9,4 +9,53 @@ fn owner_nonstatic_lifetime() {
             .next()
     }
     assert_eq!(work(&s), Some('h'));
+}
+
+#[test]
+fn nested_bowl() {
+    let a = String::from("hello");
+    let b = String::from("world");
+    let bowl = Bowl::new_box(a).map(|view, slot| {
+        let view = view.map(&slot, |a, stamp| {
+            stamp.stamp::<dyn for<'a> View<
+                'a,
+                Output = Bowl<_, dyn for<'b> View<'b, Output = (&'a String, &'b String)>>,
+            >>(Bowl::new_box(b).map(|view, slot| {
+                let view = view.map(&slot, |b, stamp| stamp.stamp((&*a, &*b)));
+                slot.fill(view)
+            }))
+        });
+        slot.fill(view)
+    });
+    let swapped_base = bowl.map(|view, slot_a| {
+        type BowlA<'b> = dyn for<'a> View<'a, Output = (&'a String, &'b String)>;
+        view.map(&slot_a, |view, stamp_a| {
+            view.map(|view, slot_b| {
+                let view = view.map(&slot_b, |view, stamp_b| {
+                    stamp_b.stamp::<dyn for<'b> View<'b, Output = ProtectedForAll<BowlA<'b>>>>(
+                        stamp_a.stamp(view),
+                    )
+                });
+                slot_b.fill(view)
+            })
+        })
+        .map(|view, slot_b| {
+            let view = view.map(&slot_b, |view, stamp_b| {
+                stamp_b
+                    .stamp::<dyn for<'b> View<'b, Output = Bowl<_, BowlA<'b>>>>(slot_a.fill(view))
+            });
+            slot_b.fill(view)
+        })
+    });
+    swapped_base.map(|view, slot| {
+        view.map(&slot, |view, _| {
+            let b = view.map(|view, slot| {
+                view.map(&slot, |(a, b), _| {
+                    assert_eq!(a, "hello");
+                    b
+                })
+            });
+            assert_eq!(b, "world");
+        })
+    });
 }
