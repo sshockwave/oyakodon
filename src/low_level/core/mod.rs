d9d73a10819d1c120dd96f729d2de43ed2185d33
Kai Su <16837579+sshockwave@users.noreply.github.com>
2026-05-09 18:38:13 +0800

    refactor: always `impl StableDeref` for `AliasableDeref`

--- src/low_level/core/mod.rs
+++ src/low_level/core/mod.rs
@@ -13,3 +13,6 @@ mod stable_deref;
 pub use self::stable_deref::*;
 #[cfg(feature = "stable_deref")]
 pub use ::stable_deref_trait::{CloneStableDeref, StableDeref};
+
+unsafe impl<T: StableDeref> StableDeref for crate::primitive::AliasableDeref<T> {}
+unsafe impl<T: CloneStableDeref> CloneStableDeref for crate::primitive::AliasableDeref<T> {}
